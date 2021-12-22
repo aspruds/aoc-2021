6afe6352cd6509aedf2b22f05ce329bdae0121ce
Andris Sprūds <adp@airbaltic.com>
2021-12-22 18:00:52 +0200

    day 3, part 1, refactoring

--- app/Services/Day3ServiceA.php
+++ app/Services/Day3ServiceA.php
@@ -18,30 +18,17 @@ class Day3ServiceA
     private function gamma_rate(array $input): int
     {
         $bit_arrays = array_map('str_split', $input);
-        $flat_bit_array = array_reduce($bit_arrays, array($this, 'bit_reducer'));
-        $sums = array_map('array_sum', $flat_bit_array);
+        $bit_arrays_transposed = array_map(null, ...$bit_arrays);
+        $sums = array_map('array_sum', $bit_arrays_transposed);
         $bits = array_map(fn($count) => $count > count($input) / 2 ? 1 : 0, $sums);
-        $binary = implode("", $bits);
-        return bindec($binary);
-    }
-
-    private function bit_reducer($carry, $item) {
-        if(!$carry) {
-            $carry = array();
-        }
-        for($pos=0; $pos < count($item); $pos++){
-            if(!isset($carry[$pos])) {
-                $carry[$pos] = array();
-            }
-            $carry[$pos][] = $item[$pos];
-        }
-        return $carry;
+        $gamma_rate_binary = implode("", $bits);
+        return bindec($gamma_rate_binary);
     }
 
     private function epsilon_rate(int $gamma_rate): int
     {
-        $gamma_bits = decbin($gamma_rate);
-        $epsilon_bits = strtr($gamma_bits,[1,0]);
-        return bindec($epsilon_bits);
+        $gamma_rate_binary = decbin($gamma_rate);
+        $epsilon_rate_binary = strtr($gamma_rate_binary,[1,0]);
+        return bindec($epsilon_rate_binary);
     }
 }
