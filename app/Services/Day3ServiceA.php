677d98c717472e42ed10a644baca23ee4910ec4e
Andris Spruds <adp@airbaltic.com>
2021-12-22 21:32:21 +0200

    day 3, part 2

--- app/Services/Day3ServiceA.php
+++ app/Services/Day3ServiceA.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use Exception;
+
 class Day3ServiceA
 {
     public function power_consumption(array $input): int
@@ -13,12 +15,22 @@ class Day3ServiceA
 
     private function gamma_rate(array $input): int
     {
+        $most_common_bits = $this->most_common_bits($input);
+        $gamma_rate_binary = implode("", $most_common_bits);
+        return bindec($gamma_rate_binary);
+    }
+
+    private function most_common_bits(array $input): array
+    {
         $bit_arrays = array_map('str_split', $input);
         $bit_arrays_transposed = array_map(null, ...$bit_arrays);
         $sums = array_map('array_sum', $bit_arrays_transposed);
-        $most_common_bits = array_map(fn($count) => $count > count($input) / 2 ? 1 : 0, $sums);
-        $gamma_rate_binary = implode("", $most_common_bits);
-        return bindec($gamma_rate_binary);
+        return array_map(fn($count) => $count >= count($input) / 2 ? 1 : 0, $sums);
+    }
+
+    private function least_common_bits(array $input): array
+    {
+        return array_map(fn($el) => 1 - $el, $this->most_common_bits($input));
     }
 
     private function epsilon_rate(int $gamma_rate): int
@@ -26,5 +38,52 @@ class Day3ServiceA
         $gamma_rate_binary = decbin($gamma_rate);
         $epsilon_rate_binary = strtr($gamma_rate_binary,[1,0]);
         return bindec($epsilon_rate_binary);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function life_support_rating(array $input): int
+    {
+        $oxygen_generator_rating = $this->oxygen_generator_rating($input);
+        $co2_scrubber_rating = $this->co2_scrubber_rating($input);
+        return $oxygen_generator_rating * $co2_scrubber_rating;
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function oxygen_generator_rating(array $input): int
+    {
+        return $this->calculate_rating($input, $this->most_common_bits(...));
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function co2_scrubber_rating(array $input): int
+    {
+        return $this->calculate_rating($input, $this->least_common_bits(...));
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function calculate_rating(array $input, $bit_counter_function): int
+    {
+        $bit_length = strlen($input[0]);
+        $filtered = $input;
+        for($i=0; $i < $bit_length; $i++) {
+            $most_common_bits = $bit_counter_function($filtered);
+            $bit = $most_common_bits[$i];
+            $filtered = array_filter($filtered, fn($el) => substr($el, $i, 1) == $bit);
+            if(count($filtered) == 1) break;
+        }
+
+        if(count($filtered) != 1) {
+            throw new Exception("filter returned more than one entry");
+        }
+        $binary_value = array_values($filtered)[0];
+        return bindec($binary_value);
     }
 }
---
+++ tests/Unit/Day3BTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Services\Day3ServiceA;
+use Exception;
+use PHPUnit\Framework\TestCase;
+
+class Day3BTest extends TestCase
+{
+    use TextReader;
+
+    protected Day3ServiceA $service;
+
+    public function setUp():void {
+        parent::setUp();
+        $this->service = new Day3ServiceA();
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function test_life_support_according_to_sample(): void
+    {
+        $input = $this->read_lines('day3-sample.txt');
+        $expected = 230;
+        $this->assertEquals($expected, $this->service->life_support_rating($input));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function test_life_support_according_to_input(): void
+    {
+        $input = $this->read_lines('day3.txt');
+        $expected = 4481199;
+        $this->assertEquals($expected, $this->service->life_support_rating($input));
+    }
+}
