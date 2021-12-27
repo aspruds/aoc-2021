c75409110a1cb2ce1da484a19be7686978d66ada
Andris Spruds <adp@airbaltic.com>
2021-12-27 20:05:43 +0200

    day 4, refactoring

--- app/Services/Day4ServiceA.php
+++ app/Services/Day4ServiceA.php
@@ -7,30 +7,33 @@ use Exception;
 
 class Day4ServiceA
 {
-    public function bingoScore(array $input): int
+    /**
+     * @throws Exception
+     */
+    public function firstBingoScore(array $input): int
     {
-        $bingoNumbers = explode(",", $input[0]);
-        $bingoBoards = $this->constructBoards($input);
-
-        $bingoScore = 0;
-        foreach($bingoNumbers as $calledNumber) {
-            foreach($bingoBoards as $board) {
-                $board->markNumbers($calledNumber);
-                if($board->isWinning()) {
-                    $bingoScore = $board->sumUnmarked() * $calledNumber;
-                    break 2;
-                }
-            }
-        }
-        return $bingoScore;
+        $scores = $this->bingoScores($input);
+        return reset($scores);
     }
 
+    /**
+     * @throws Exception
+     */
     public function lastBoardBingoScore(array $input): int
+    {
+        $scores = $this->bingoScores($input);
+        return end($scores);
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function bingoScores($input): array
     {
         $bingoNumbers = explode(",", $input[0]);
         $bingoBoards = $this->constructBoards($input);
 
-        $bingoScore = 0;
+        $bingoScores = array();
         $winningBoards = array();
 
         foreach($bingoNumbers as $calledNumber) {
@@ -41,15 +44,15 @@ class Day4ServiceA
                         continue;
                     }
                     $winningBoards[] = $board->getId();
-
-                    if(count($winningBoards) == count($bingoBoards)) {
-                        $bingoScore = $board->sumUnmarked() * $calledNumber;
-                        break 2;
-                    }
+                    $bingoScores[] = $board->sumUnmarked() * $calledNumber;
                 }
             }
         }
-        return $bingoScore;
+
+        if(count($bingoScores) == 0) {
+            throw new Exception("there are no winning boards");
+        }
+        return $bingoScores;
     }
 
     private function constructBoards($input): array {
@@ -62,6 +65,4 @@ class Day4ServiceA
         }
         return $boards;
     }
-
-
 }
--- tests/Unit/Day4ATest.php
+++ tests/Unit/Day4ATest.php
@@ -16,17 +16,23 @@ class Day4ATest extends TestCase
         $this->service = new Day4ServiceA();
     }
 
+    /**
+     * @throws \Exception
+     */
     public function test_bingo_winning_score_to_sample(): void
     {
         $input = $this->read_lines('day4-sample.txt');
         $expected = 4512;
-        $this->assertEquals($expected, $this->service->bingoScore($input));
+        $this->assertEquals($expected, $this->service->firstBingoScore($input));
     }
 
+    /**
+     * @throws \Exception
+     */
     public function test_bingo_winning_score_to_input(): void
     {
         $input = $this->read_lines('day4.txt');
         $expected = 23177;
-        $this->assertEquals($expected, $this->service->bingoScore($input));
+        $this->assertEquals($expected, $this->service->firstBingoScore($input));
     }
 }
